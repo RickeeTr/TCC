b484a6fa8889de08a29db051fc5ce4372b7ccfd7
Rickee <s.trindade.erick@gmail.com>
2025-11-13 01:38:45 -0300

    Add files via upload

--- extra/dados.php
+++ extra/dados.php
@@ -1,6 +1,10 @@
 <?php
-header('Content-Type: application/json'); // Retorna JSON
+header('Content-Type: application/json');
 
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 23 Jul 2000 05:00:00 GMT");
+
+// CONASEMS
 $apiKey = 'AIzaSyDjA3Lm1d3BrDmSsOthT9hg8W5t-26lzY0';
 $channelId = 'UCKy854A2wVaBkZO9kAt1w_g';
 
@@ -8,18 +12,15 @@ $url = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=$chann
 $response = file_get_contents($url);
 $data = json_decode($response, true);
 
-if (isset($data['items'][0]['statistics'])) {
-    $inscritos = floor($data['items'][0]['statistics']['subscriberCount'] / 1000);
-    $videos = floor($data['items'][0]['statistics']['videoCount'] / 1000);
+$resultado = [
+    'inscritos' => 0,
+    'videos' => 0
+];
 
-    echo json_encode([
-        'inscritos' => $inscritos,
-        'videos' => $videos
-    ]);
-} else {
-    echo json_encode([
-        'inscritos' => 0,
-        'videos' => 0
-    ]);
+if (isset($data['items'][0]['statistics'])) {
+    $resultado['inscritos'] = floor($data['items'][0]['statistics']['subscriberCount'] / 1000);
+    $resultado['videos'] = floor($data['items'][0]['statistics']['videoCount'] / 1000);
 }
+
+echo json_encode($resultado);
 ?>
